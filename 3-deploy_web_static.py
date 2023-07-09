ebfabaca8f870ec2ab2047a669d0ca2b52ef6194
6godpro <erickson5115@gmail.com>
2023-07-09 14:22:27 +0100

    initial commit task 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+""""""
+
+import os.path
+from datetime import datetime
+from fabric.api import (
+    env,
+    local,
+    put,
+    run,
+    task,
+    execute
+)
+
+env.hosts = ['52.87.215.172', '100.26.231.127']
+shared_file = None
+
+
+def do_pack():
+    """Creates a .tgz archive file."""
+    dt = datetime.strftime(datetime.utcnow(), '%Y%m%d%H%M%S')
+    file_path = "versions/web_static_{}.tgz".format(dt)
+    if not os.path.exists('versions'):
+        if local('mkdir -p versions').return_code != 0:
+            return None
+    command = "tar -cvzf {} web_static".format(file_path)
+    if local(command).return_code != 0:
+        return None
+    return file_path
+
+
+def do_deploy(archive_path):
+    """Deploys an archive to the web servers."""
+    if os.path.isfile(archive_path) is False:
+        return False
+
+    file = archive_path.split('/')[-1].split('.')[0]
+    r_path = "/data/web_static/releases"
+
+    if put(archive_path, '/tmp/{}.tgz'.format(file)).failed is True:
+        return False
+
+    if run('rm -rf {}/{}'.format(r_path, file)).failed is True:
+        return False
+
+    if run('mkdir -p {}/{}/'.format(r_path, file)).failed is True:
+        return False
+
+    if run('tar -xzf /tmp/{}.tgz -C {}/{}/'
+           .format(file, r_path, file)).failed is True:
+        return False
+
+    if run('rm /tmp/{}.tgz'.format(file)).failed is True:
+        return False
+
+    command = "mv {}/{}/web_static/*".format(r_path, file)
+    command += " {}/{}/".format(r_path, file)
+    if run(command).failed is True:
+        return False
+
+    if run('rm -rf {}/{}/web_static'
+           .format(r_path, file)).failed is True:
+        return False
+
+    if run('rm -rf /data/web_static/current').failed is True:
+        return False
+
+    if run('ln -s {}/{} /data/web_static/current'
+           .format(r_path, file)).failed is True:
+        return False
+    return True
+
+
+def deploy():
+    """Creates and distributes an archive to web servers."""
+
+    global shared_file
+    deploy.flag = getattr(deploy, 'flag', False)
+    if deploy.flag is False:
+        shared_file = do_pack()
+        deploy.flag = True
+    if shared_file is None:
+        return False
+
+    return do_deploy(shared_file)
